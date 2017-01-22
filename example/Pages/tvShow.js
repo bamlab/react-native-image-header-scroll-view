525fa3d504fd19fa2e343cc73c01cebbb40c3b3c
Nicolas Djambazian <nicolas@djambazian.fr>
2017-01-22 15:37:15 +0100

    Prepare for more example

---
+++ example/Pages/tvShow.js
@@ -0,0 +1,162 @@
+import Exponent from 'exponent';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  StatusBar,
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import { NavigationBar } from '@exponent/ex-navigation';
+
+
+// import HeaderImageScrollView, { TriggeringView } from 'react-native-image-header-scroll-view';
+import HeaderImageScrollView, { TriggeringView } from '../ImageHeaderScrollView';
+import tvShowContent from '../assets/tvShowContent';
+
+const MIN_HEIGHT = NavigationBar.DEFAULT_HEIGHT;
+const MAX_HEIGHT = 250;
+
+const styles = StyleSheet.create({
+  image: {
+    height: MAX_HEIGHT,
+    width: Dimensions.get('window').width,
+    alignSelf: 'stretch',
+    resizeMode: 'cover',
+  },
+  title: {
+    fontSize: 20,
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+  section: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    backgroundColor: 'white',
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  sectionContent: {
+    fontSize: 16,
+    textAlign: 'justify',
+  },
+  keywords: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    flexWrap: 'wrap',
+  },
+  keywordContainer: {
+    backgroundColor: '#999999',
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+  },
+  keyword: {
+    fontSize: 16,
+    color: 'white',
+  },
+  titleContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageTitle: {
+    color: 'white',
+    backgroundColor: 'transparent',
+    fontSize: 24,
+  },
+  navTitleView: {
+    height: MIN_HEIGHT,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 16,
+    opacity: 0,
+  },
+  navTitle: {
+    color: 'white',
+    fontSize: 18,
+    backgroundColor: 'transparent',
+  },
+});
+
+class TvShow extends Component {
+  constructor() {
+    super();
+    this.state = { showNavTitle: false };
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, marginTop: -MIN_HEIGHT }}>
+        <StatusBar barStyle="light-content" />
+        <HeaderImageScrollView
+          maxHeight={MAX_HEIGHT}
+          minHeight={MIN_HEIGHT}
+          maxOverlayOpacity={0.6}
+          minOverlayOpacity={0.3}
+          fadeOutForeground
+          renderHeader={() => (
+            <Image source={tvShowContent.image} style={styles.image} />
+          )}
+          renderFixedForeground={() => (
+            <Animatable.View
+              style={styles.navTitleView}
+              ref={(navTitleView) => { this.navTitleView = navTitleView; }}
+            >
+              <Text style={styles.navTitle}>{tvShowContent.title}, ({tvShowContent.year})</Text>
+            </Animatable.View>
+          )}
+          renderForeground={() => (
+            <View style={styles.titleContainer}>
+              <Text style={styles.imageTitle}>{tvShowContent.title}</Text>
+            </View>
+          )}
+        >
+          <TriggeringView
+            style={styles.section}
+            onHide={() => this.navTitleView.fadeInUp(200)}
+            onDisplay={() => this.navTitleView.fadeOut(100)}
+          >
+            <Text style={styles.title}>
+              <Text style={styles.name}>{tvShowContent.title}</Text>, ({tvShowContent.year})
+            </Text>
+          </TriggeringView>
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Overview</Text>
+            <Text style={styles.sectionContent}>{tvShowContent.overview}</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Keywords</Text>
+            <View style={styles.keywords}>
+              {tvShowContent.keywords.map(keyword => (
+                <View style={styles.keywordContainer} key={keyword}>
+                  <Text style={styles.keyword}>{keyword}</Text>
+                </View>
+              ))}
+            </View>
+          </View>
+        </HeaderImageScrollView>
+      </View>
+    );
+  }
+}
+
+TvShow.route = {
+  navigationBar: {
+    tintColor: 'white',
+    backgroundColor: 'transparent',
+    borderBottomWidth: 0,
+    statusBarHeight: 0,
+    elevation: 0,
+  },
+};
+
+export default TvShow;
--- example/main.js
+++ example/main.js
@@ -1,148 +1,33 @@
-import Exponent from 'exponent';
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  StyleSheet,
+  AppRegistry,
   Text,
   View,
-  Image,
-  Dimensions,
-  StatusBar,
 } from 'react-native';
-import * as Animatable from 'react-native-animatable';
 
-import HeaderImageScrollView, { TriggeringView } from 'react-native-image-header-scroll-view';
-import tvShowContent from './assets/tvShowContent';
+import Exponent from 'exponent';
 
-const MIN_HEIGHT = 66;
-const MAX_HEIGHT = 250;
+import {
+  createRouter,
+  NavigationProvider,
+  StackNavigation,
+} from '@exponent/ex-navigation';
 
-class TvShow extends Component {
-  constructor() {
-    super();
-    this.state = { showNavTitle: false };
-  }
+import { Menu, TvShow } from './Pages';
 
+export const Router = createRouter(() => ({
+  menu: () => Menu,
+  tvShow: () => TvShow,
+}));
+
+class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <StatusBar barStyle="light-content"/>
-        <HeaderImageScrollView
-          maxHeight={MAX_HEIGHT}
-          minHeight={MIN_HEIGHT}
-          maxOverlayOpacity={0.6}
-          minOverlayOpacity={0.3}
-          fadeOutForeground
-          renderHeader={() => (
-            <Image source={tvShowContent.image} style={styles.image} />
-          )}
-          renderFixedForeground={() => (
-            <Animatable.View
-              style={styles.navTitleView}
-              ref={(navTitleView) => { this.navTitleView = navTitleView }}
-            >
-              <Text style={styles.navTitle}>{tvShowContent.title}, ({tvShowContent.year})</Text>
-            </Animatable.View>
-          )}
-          renderForeground={() => (
-            <View style={styles.titleContainer}>
-              <Text style={styles.imageTitle}>{tvShowContent.title}</Text>
-            </View>
-          )}
-        >
-          <TriggeringView
-            style={styles.section}
-            onHide={() => this.navTitleView.fadeInUp(200)}
-            onDisplay={() => this.navTitleView.fadeOut(100)}
-          >
-            <Text style={styles.title}>
-              <Text style={styles.name}>{tvShowContent.title}</Text>, ({tvShowContent.year})
-            </Text>
-          </TriggeringView>
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Overview</Text>
-            <Text style={styles.sectionContent}>{tvShowContent.overview}</Text>
-          </View>
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Keywords</Text>
-            <View style={styles.keywords}>
-              {tvShowContent.keywords.map((keyword) => (
-                <View style={styles.keywordContainer} key={keyword}>
-                  <Text style={styles.keyword}>{keyword}</Text>
-                </View>
-              ))}
-            </View>
-          </View>
-        </HeaderImageScrollView>
-      </View>
+      <NavigationProvider router={Router}>
+        <StackNavigation initialRoute={Router.getRoute('menu')} />
+      </NavigationProvider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  image: {
-    height: MAX_HEIGHT,
-    width: Dimensions.get('window').width,
-    alignSelf: 'stretch',
-    resizeMode: 'cover',
-  },
-  title: {
-    fontSize: 20,
-  },
-  name: {
-    fontWeight: 'bold',
-  },
-  section: {
-    padding: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-  },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  sectionContent: {
-    fontSize: 16,
-    textAlign: 'justify',
-  },
-  keywords: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    flexWrap: 'wrap',
-  },
-  keywordContainer: {
-    backgroundColor: '#999999',
-    borderRadius: 10,
-    margin: 10,
-    padding: 10,
-  },
-  keyword: {
-    fontSize: 16,
-    color: 'white',
-  },
-  titleContainer: {
-    flex: 1,
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  imageTitle: {
-    color: 'white',
-    backgroundColor: 'transparent',
-    fontSize: 24,
-  },
-  navTitleView: {
-    height: MIN_HEIGHT,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 8,
-    opacity: 0,
-  },
-  navTitle: {
-    color: 'white',
-    fontSize: 18,
-    backgroundColor: 'transparent',
-  },
-});
-
-Exponent.registerRootComponent(TvShow);
+Exponent.registerRootComponent(App);
