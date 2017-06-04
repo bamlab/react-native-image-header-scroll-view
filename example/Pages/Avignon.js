63d4f04b10105b9cc5a1fafac105bae355f713dd
Nicolas Djambazian <nicolas@djambazian.fr>
2017-06-04 15:15:14 +0200

    Add a touchableFixedForeground

---
+++ example/Pages/Avignon.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { StyleSheet, Text, Image, TouchableOpacity, View, Dimensions } from 'react-native';
+import { NavigationBar } from '@exponent/ex-navigation';
+
+import HeaderImageScrollView, { TriggeringView } from 'react-native-image-header-scroll-view';
+
+const MIN_HEIGHT = NavigationBar.DEFAULT_HEIGHT;
+const MAX_HEIGHT = 200;
+
+class BasicUsage extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <HeaderImageScrollView
+          maxHeight={MAX_HEIGHT}
+          minHeight={MIN_HEIGHT}
+          minOverlayOpacity={0.4}
+          renderHeader={() => (
+            <Image source={require('../assets/avignon.jpg')} style={styles.image} />
+          )}
+          renderTouchableFixedForeground={() => (
+            <View style={{ height: MAX_HEIGHT, justifyContent: 'center', alignItems: 'center' }}>
+              <TouchableOpacity onPress={() => console.log('tap!!')} style={styles.button}>
+                <Text style={styles.buttonText}>Discover Avignon now!</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        >
+          <View style={{ height: 1000 }}>
+            <TriggeringView onHide={() => console.log('text hidden')}>
+              <TouchableOpacity onPress={() => console.log('tap!!')} style={styles.button}>
+                <Text style={styles.buttonText}>Discover Another city</Text>
+              </TouchableOpacity>
+            </TriggeringView>
+          </View>
+        </HeaderImageScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: -MIN_HEIGHT,
+    flex: 1,
+  },
+  image: {
+    height: MAX_HEIGHT,
+    width: Dimensions.get('window').width,
+  },
+  button: {
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderColor: 'white',
+    backgroundColor: '#00000066',
+  },
+  buttonText: {
+    color: 'white',
+    backgroundColor: 'transparent',
+  },
+});
+
+BasicUsage.route = {
+  navigationBar: {
+    tintColor: 'white',
+    backgroundColor: 'transparent',
+    borderBottomWidth: 0,
+    statusBarHeight: 0,
+    elevation: 0,
+  },
+};
+
+export default BasicUsage;
--- example/Pages/index.js
+++ example/Pages/index.js
@@ -2,3 +2,4 @@ export { default as Menu } from './Menu';
 export { default as TvShow } from './TvShow';
 export { default as BasicUsage } from './BasicUsage';
 export { default as ColorsPage } from './Colors';
+export { default as Avignon } from './Avignon';
