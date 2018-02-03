a58edd356d190ebd14e95e1e4ffea90e4a5b933a
Nicolas Djambazian <nicolas@djambazian.fr>
2018-02-03 16:02:53 +0100

    REmove example

--- example/Pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Menu } from './Menu';
-export { default as TvShow } from './TvShow';
-export { default as BasicUsage } from './BasicUsage';
-export { default as ColorsPage } from './Colors';
-export { default as Avignon } from './Avignon';
-export { default as PullToRefresh } from './PullToRefresh';
--- example/index.android.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './main';
-
-AppRegistry.registerComponent('imageHeaderScrollView', () => App);
